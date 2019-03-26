2b8f4ff38172000d774ade3b9f1c4bd3858e3dc4
dhirajko <dhirajkoirala2013@gmail.com>
2019-03-26 20:08:07 +0200

    client card added css incomplete

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const app = express();
 const bodyParser = require('body-parser')
 const db = require('./startup/databae_config')
 const image = require('./router/image_form');
+const cors=require('cors')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -21,6 +24,6 @@ app.post('/', (req, res) => {
     res.send('Image uploaded post')
 });
 db;
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log('running on port  3000');
 })
